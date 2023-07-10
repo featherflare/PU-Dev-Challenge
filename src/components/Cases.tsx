0357c6136cbe5720248ac863d507758080427c26
featherflare <max43607@gmail.com>
2023-07-10 20:23:17 +0700

    Update : css format

--- src/components/Cases.tsx
+++ src/components/Cases.tsx
@@ -1,19 +1,21 @@
 export default function Cases() {
   return (
-    <section>
-      <div className='Inter'>จำนวนคดีที่เกิดขึ้น</div>
-      <div className='IBM'>
-        มาตรา 112 มีการตีความการกระทำอย่างไร้ขอบเขต
-        แต่ทุกคนสามารถผู้กล่าวโทษให้ดำเนินคดีได้
-        จึงมีการกล่าวหากันเป็นจำนวนมากในช่วงที่มีความขัดแย้งทางการเมืองหลังจากปี
-        2548 เป็นต้นมา
-      </div>
-      <div></div>
-      <div className='IBM'>
-        ที่มา:{' '}
-        <span>
-          <a>iLaw</a>
-        </span>
+    <section className='section invert'>
+      <div className='margin'>
+        <div className='IBM header2'>จำนวนคดีที่เกิดขึ้น</div>
+        <div className='IBM body padding2'>
+          มาตรา 112 มีการตีความการกระทำอย่างไร้ขอบเขต
+          แต่ทุกคนสามารถผู้กล่าวโทษให้ดำเนินคดีได้
+          จึงมีการกล่าวหากันเป็นจำนวนมากในช่วงที่มีความขัดแย้งทางการเมืองหลังจากปี
+          2548 เป็นต้นมา
+        </div>
+        <div></div>
+        <div className='IBM body2 center'>
+          ที่มา:{' '}
+          <span>
+            <a href='https://freedom.ilaw.or.th/node/817'>iLaw</a>
+          </span>
+        </div>
       </div>
     </section>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
-    <section className='section1'>
-      <div className='IBM header'>ม.๑๑๒</div>
-      <div className='IBM detail'>
+    <section className='section'>
+      <div className='IBM header center'>ม.๑๑๒</div>
+      <div className='IBM body center margin padding1'>
         “ผู้ใดหมิ่นประมาท ดูหมิ่น หรือแสดงความอาฆาตมาดร้าย พระมหากษัตริย์
         พระราชินี รัชทายาท หรือผู้สำเร็จราชการแทนพระองค์
         ต้องระวางโทษจำคุกตั้งแต่สามปีถึงสิบห้าปี”
