9b21c0e8399b88a6d0c0ecdbe7e8faaad6419488
featherflare <max43607@gmail.com>
2023-07-10 22:14:56 +0700

    Add :animation in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <section className='section flex'>
-      <div className='IBM header center'>ม.๑๑๒</div>
-      <div className='IBM body center margin padding1'>
+      <div className='IBM header center typed margin'>ม.๑๑๒</div>
+      <div className='IBM body center margin padding1 delays1 fade'>
         “ผู้ใดหมิ่นประมาท ดูหมิ่น หรือแสดงความอาฆาตมาดร้าย พระมหากษัตริย์
         พระราชินี รัชทายาท หรือผู้สำเร็จราชการแทนพระองค์
         ต้องระวางโทษจำคุกตั้งแต่สามปีถึงสิบห้าปี”
